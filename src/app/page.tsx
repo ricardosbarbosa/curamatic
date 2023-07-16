5c35e5a66e2cc1a1a202f0d5600650c27ece5aa3
Ricardo Barbosa <rbrico@gmail.com>
2023-07-15 22:17:15 -0300

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
+      <h1>Home</h1>
     </div>
   )
 }
