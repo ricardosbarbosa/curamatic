b7c799352bb16baecfa8033be0486cea8325d4ce
Ricardo Barbosa <rbrico@gmail.com>
2023-07-15 22:27:25 -0300

    clean up

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React, { Fragment } from 'react'
 import { navigation } from '../lib/navigation'
+import { usePathname } from 'next/navigation'
 
 type Props = {
   sidebarOpen: boolean
@@ -15,6 +16,10 @@ export default function MobileMenu({
   sidebarOpen = false,
   setSidebarOpen = () => { },
 }: Props) {
+
+  const pathname = usePathname()
+
+  const itemCurrent = navigation.find((nav) => pathname.includes(nav.href))
 
   return (
     <>
@@ -79,7 +84,7 @@ export default function MobileMenu({
                               <Link
                                 href={item.href}
                                 className={classNames(
-                                  item.current
+                                  itemCurrent?.name === item.name
                                     ? 'bg-gray-50 text-sky-600'
                                     : 'text-gray-700 hover:text-sky-600 hover:bg-gray-50',
                                   'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
@@ -88,7 +93,7 @@ export default function MobileMenu({
                               >
                                 <item.icon
                                   className={classNames(
-                                    item.current ? 'text-sky-600' : 'text-gray-400 group-hover:text-sky-600',
+                                    itemCurrent?.name === item.name ? 'text-sky-600' : 'text-gray-400 group-hover:text-sky-600',
                                     'h-6 w-6 shrink-0'
                                   )}
                                   aria-hidden="true"
